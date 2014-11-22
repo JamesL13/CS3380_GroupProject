817ed7f9032281626d5f90843bf902201d5b9a67
jmlmdf <jmlmdf@mail.missouri.edu>
2014-11-21 20:40:35 -0600

    Updated file
    This is the updated Order file with the added radio buttons.

--- web_files/Order.php
+++ web_files/Order.php
@@ -6,21 +6,21 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <link href="main.css" rel="stylesheet">
     <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-    <script type="text/javascript" src="js/jquery.panelgallery-2.0.0.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/ajax.js"></script>
+    <script type="text/javascript" src="jquery.panelgallery-2.0.0.min.js"></script>
+    <script src="bootstrap.min.js"></script>
+    <script src="ajax.js"></script>
     <link href='http://fonts.googleapis.com/css?family=Exo+2:400,800' rel='stylesheet' type='text/css'>
 
     <title>Ordering</title>
 </head>
 <body>
-	<div class="TonysBanner">
-		<img id="TonysImg" src="images/TonysPizzaBanner.png" alt="Tonys" height="82px" width="644px">
-	</div>
+    <div class="TonysBanner">
+        <img id="TonysImg" src="images/TonysPizzaBanner.png" alt="Tonys" height="82px" width="644px">
+    </div>
     <div class="col-md-12">
         <nav class="navbar navbar-inverse" role="navigation">
             <div class="container-fluid">
@@ -47,16 +47,54 @@
             </div><!-- /.container-fluid -->
     </div><!--col-md-12-->
     </nav>
-    <h1>Tony's Pizza</h1>
-    <hr />
+    <div id="OrderBox">
     <div>
         <form method="POST" action="Order.php">
-            <input type="checkbox" name="Special1" value="Special1">Special 1<br />
-            <input type="checkbox" name="Special2" value="Special2">Special 2<br />
-            <input type="checkbox" name="Special3" value="Special3">Special 3<br />
+
+            <input type="radio" name="Small" values="Small">Small<br />
+            <input type="radio" name="Medium" values="Medium">Medium<br />
+            <input type="radio" name="Large" values="Large">Large<br />
+
+            <hr ><!--Dividing Line-->
+
+            <input type="checkbox" name="Onions" value="Onions">Onions<br />
+            <input type="checkbox" name="Mushrooms" value="Mushrooms">Mushrooms<br />
+            <input type="checkbox" name="Canadian_Bacon" value="Canadian_Bacon">Canadian Bacon<br />
+            <input type="checkbox" name="Sausage" value="Sausage">Sausage<br />
+            <input type="checkbox" name="Beef" value="Beef">Beef<br />
+            <input type="checkbox" name="Anchovies" value="Anchovies">Anchovies<br />
+            <input type="checkbox" name="Pepperoni" value="Pepperoni">Pepperoni<br />
+            <input type="checkbox" name="Bacon" value="Bacon">Bacon<br />
+            <input type="checkbox" name="Pepperjack Cheese" value="Pepperjack Cheese">Pepperjack Cheese<br />
+            <input type="checkbox" name="Chicken" value="Chicken">Chicken<br />
+            <input type="checkbox" name="Black Olive" value="Black Olive">Black Olive<br />
+            <input type="checkbox" name="Jalapeno" value="Jalapeno">Jalapeno<br />
+            <input type="checkbox" name="Shrimp" value="Shrimp">Shrimp<br />
+            <input type="checkbox" name="Salami" value="Salami">Salami<br />
+            <input type="checkbox" name="Green_Peppers" value="Green_Peppers">Green Peppers<br />
+            <input type="checkbox" name="Pineapple" value="Pineapple">Pineapple<br />
+            <input type="checkbox" name="Green_Olive" value="Green_Olive">Green Olive<br />
+            <input type="checkbox" name="Gyros_Meat" value="Gyros_Meat">Gyros Meat<br />
+
+            <hr ><!--Dividing Line-->
+
+            <input type="radio" name="Veggie" value="Veggie">Veggie<br />
+            <input type="radio" name="Meat_Lovers" value="Meat_Lovers">Meat_Lovers<br />
+            <input type="radio" name="Tonys_Special" value="Tonys_Special">Tony's Special<br />
+            <input type="radio" name="House_Special" value="House_Special">House Special<br />
+            <input type="radio" name="The_Zeus" value="The_Zeus">The Zeus<br />
+
+            <hr><!--Dividing Line-->
+
+            <input type="checkbox" name="Fountain_Drinks" value="Fountain_Drinks">Fountain Drinks<br />
+            <input type="checkbox" name="Soda_Pitcher" value="Soda_Pitcher">Soda Pitcher<br />
+            <input type="checkbox" name="Bottle_Drinks" value="Bottle_Drinks">Bottle Drinks<br />
+
+            <br />
             <input type="submit" name="submit" value="Execute" />
         </form>
     </div>
+    </div><!--Order Box-->
 
 </body>
 </html>
@@ -72,13 +110,18 @@ if (!$conn)
 {
     die('Fail');
 }
+else
+{
+    echo "Success";
+}
 
 if(isset($_POST['submit']))
 {
-    $result = pg_query('SELECT name FROM TonysPizza.specialtyPizza') or die ('Could not find pizza' . pg_last_error());
+    echo "This works";
+    $result = pg_query('SELECT name FROM TonysPizza.specialtypizza WHERE id = 1') or die ('Could not find pizza' . pg_last_error());
 
     $line = pg_fetch_array($result, null, PGSQL_ASSOC);
-    echo $line;
+    
     echo '<i> $line <\i> Is what was returned';
 }
 ?>
