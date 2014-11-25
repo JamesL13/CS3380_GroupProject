b41d73f8e033283fc042629c6bdd104e72250e70
jmlmdf <jmlmdf@mail.missouri.edu>
2014-11-25 16:24:41 -0600

    Adds the new page and the iframe on the order.php

---
+++ web_files/CurrentOrder.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+ <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.panelgallery-2.0.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/ajax.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Exo+2:400,800' rel='stylesheet' type='text/css'>
+
+
+</head>
+
+<body>
+	<div class="Box">
+	TEST
+	</div>
+</body>
+
+</html>
--- web_files/Order.php
+++ web_files/Order.php
@@ -60,6 +60,11 @@ if(empty($_SESSION['username'])){
             </div><!-- /.container-fluid -->
     </div><!--col-md-12-->
     </nav>
+    <div>
+    <iframe src="CurrentOrder.php">
+
+    </iframe>
+    </div>
     <div id="OrderBox">
     <div>
         <form method="POST" action="Order.php">
@@ -108,10 +113,6 @@ if(empty($_SESSION['username'])){
         </form>
     </div>
     </div><!--Order Box-->
-    <iframe src="http://www.w3schools.com">
-
-    </iframe>
-
 </body>
 </html>
 
